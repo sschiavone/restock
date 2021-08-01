1d516bb065b7785d57680d426c35f7dcf8027bca
Scott Schiavone <schiavonescott@gmail.com>
2021-08-01 18:37:50 -0500

    got inventory count working

--- restock.py
+++ restock.py
@@ -1,10 +1,28 @@
 import json
-from square.client import Client
+from   square.client import Client
 
 client = Client(
     access_token = 'EAAAEFv7_mfYuGUqCp8zYFqPQb5nPULMjLd7tNZa0qW4KgmN3oItAj-mrB1T25YR',
-    environment='sandbox'
+    environment  = 'sandbox'
 )
+
+cat_print = '2VPZMNWFHYKEFVPM4L5EX6VW'
+
+restock_list = []
+
+def check_item_stock(item, min_stock):
+    data = item['item_variation_data']
+    stock = client.inventory.retrieve_inventory_count(item['id'])
+
+    if stock.is_success():
+        try:
+            quantity = stock.body['counts'][0]['quantity']
+
+            if int(quantity) <= min_stock:
+                restock_list.append(item)
+                #print(data['name'], data['sku'], quantity)
+        except:
+            pass
 
 
 def get_catalog():
@@ -13,28 +31,21 @@ def get_catalog():
     if catalog.is_success():
         for catalog_item in catalog.body['objects']:
     
-            item_data   = catalog_item['item_data']
+            item_data = catalog_item['item_data']
             try:
-                item_category = item_data['category_id']
-                item_name   = item_data['name']
+                if item_data['category_id'] == cat_print:
+                    for item in item_data['variations']:
+                        check_item_stock(item, 1)
 
-                item_id     = item_data['variations'][0]['id']
-
-                print(item_category)
             except:
                 pass
 
-            #for item in item_data['variations']:
-                #print(item['id'])
-            
-    
-            #print(item_name, item_id)
-            #stock  = client.inventory.retrieve_inventory_count(item_id)
-            #if stock.is_success():
-            #    print(item_name, stock.body['counts'][0]['quantity'])
-    
     elif catalog.is_error():
         print(catalog.errors)
 
 get_catalog()
+
+print(restock_list)
+
+
 
