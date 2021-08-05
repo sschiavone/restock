afd7c5ac3c05f96c38ec2ad2d2062fd98c0b52e3
Scott Schiavone <schiavonescott@gmail.com>
2021-08-05 16:08:17 -0500

    changed a few things

--- restock.py
+++ restock.py
@@ -1,14 +1,13 @@
 import json
-from   square.client import Client
+from square.client import Client
 
 client = Client(
-    access_token = 'EAAAEFv7_mfYuGUqCp8zYFqPQb5nPULMjLd7tNZa0qW4KgmN3oItAj-mrB1T25YR',
-    environment  = 'sandbox'
+    access_token='EAAAEFv7_mfYuGUqCp8zYFqPQb5nPULMjLd7tNZa0qW4KgmN3oItAj-mrB1T25YR',
+    environment='sandbox'
 )
 
 cat_print = '2VPZMNWFHYKEFVPM4L5EX6VW'
 
-restock_list = []
 
 def check_item_stock(item, min_stock):
     data = item['item_variation_data']
@@ -19,33 +18,33 @@ def check_item_stock(item, min_stock):
             quantity = stock.body['counts'][0]['quantity']
 
             if int(quantity) <= min_stock:
-                restock_list.append(item)
-                #print(data['name'], data['sku'], quantity)
+                return True
+            else:
+                return False
         except:
-            pass
+            return False
 
 
-def get_catalog():
+def get_low_stock():
     catalog = client.catalog.list_catalog(types='ITEM')
 
     if catalog.is_success():
+        restock_list = []
         for catalog_item in catalog.body['objects']:
-    
+
             item_data = catalog_item['item_data']
             try:
                 if item_data['category_id'] == cat_print:
                     for item in item_data['variations']:
-                        check_item_stock(item, 1)
+                        if check_item_stock(item, 1):
+                            restock_list.append(item)
 
             except:
                 pass
+        return restock_list
 
     elif catalog.is_error():
         print(catalog.errors)
 
-get_catalog()
 
-print(restock_list)
-
-
-
+print(get_low_stock())
